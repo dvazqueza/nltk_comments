43bccdafc4a8a4bb44234ccd24e88e457a1e5faf
Daniel Vazquez Avilés <dvazqueza@gmail.com>
2016-11-08 09:38:00 -0600

    Modifications on comments

--- ntlk_sample.py
+++ ntlk_sample.py
@@ -1,19 +1,43 @@
 import nltk
 from nltk.corpus import treebank
 
+def sentenceAnalysis(sentence):
+    tokens = nltk.word_tokenize(sentence)
+
+    print(tokens)
+    tagged = nltk.pos_tag(tokens)
+    tagged[0:6]
+    print(tagged)
+    entities = nltk.chunk.ne_chunk(tagged)
+    print(entities)
+
+
 sentence = """At eight o'clock on Thursday morning
 ... Arthur didn't feel very good."""
-tokens = nltk.word_tokenize(sentence)
+sentenceAnalysis(sentence)
 
-print (tokens)
 
-tagged = nltk.pos_tag(tokens)
-tagged[0:6]
+comments_file= open('C:\\Users\\dvazquez\\PycharmProjects\\nltk_comments\\nltk_comments\\Comments\\SOURCE\\Form3.txt')
+raw = comments_file.read()
+print (type(raw))
+print (len(raw))
+print (raw[:75])
 
-print (tagged)
 
-entities = nltk.chunk.ne_chunk(tagged)
-print (entities)
+sentence_tokens = nltk.sent_tokenize(raw)
+print (type(sentence_tokens))
+print (len(sentence_tokens))
 
-t= treebank.parsed_sents('C:\\Users\\dvazquez\\Google Drive\\PycharmProjects\\GGComments\\news.txt')[0]
-t.draw()
+for index, sentence in enumerate(sentence_tokens):
+    if (sentence.startswith('Exam Grade:')
+        or sentence.startswith('Term Grade:')
+        or sentence.startswith('Practical Exam Grade:')
+        or sentence.startswith('Theory Exam Grade:')):
+        # print (index, sentence)
+        sentence_tokens.pop(index)
+    else :
+        print (sentence)
+        sentenceAnalysis(sentence)
+
+
+
